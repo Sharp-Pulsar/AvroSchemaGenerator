8a6789df0dbd404b4c25d9c74f5a54732ebe30e8
eabanonu <eabanonu@yahoo.com>
2020-06-25 19:36:12 +0100

    [WIP] Aliases

---
+++ SchemaGenerator/Attributes/Aliases.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AvroSchemaGenerator.Attributes
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class AliasesAttribute : Attribute
+    {
+        private readonly string _value;
+
+        public AliasesAttribute(string value)
+        {
+            _value = value;
+        }
+        public virtual string Value => _value;
+    }
+}
